fc227c8163b6ed2d358da1cd0c364e6da5903b9b
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2023-04-01 18:21:28 -0400

    Remove unused imports

--- src/main/java/ca/warp7/frc2023/subsystems/LimelightSubsystem.java
+++ src/main/java/ca/warp7/frc2023/subsystems/LimelightSubsystem.java
@@ -1,11 +1,10 @@
 package ca.warp7.frc2023.subsystems;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -61,5 +60,3 @@ public class LimelightSubsystem extends SubsystemBase {
         yaw = botposeTargetspace[5];
     }
 }
-
-
